a9cc9ccf40466bd3e649414948609abe0d58ea5c
unknown <ismailagboola130@gmail.com>
2024-08-26 17:05:19 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+// // 1, Writh a C# program to find the sum of a givin positive integer.
+// Console.WriteLine("User Input");
+// int userInput = Convert.ToInt32(Console.ReadLine());
+// if ( userInput > 0 ) 
+// {
+// Console.WriteLine("this is a positive number");
+// }
+
+
+
+
+
+
+// 2, Write a C# program that takes two integer input from the user and displays their sum.
+// Console.WriteLine("Input Your First Number");
+// int userInput1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input your Second Number");
+// int userInput2 = Convert.ToInt32(Console.ReadLine());
+// int Sum = userInput1 + userInput2;
+// Console.WriteLine("");
+
+
+
+// // 3, Write a progrm that checks if a given integer is even or odd
+// Console.WriteLine("Whole Number");
+// int WholeNumber = Convert.ToInt32(Console.ReadLine());
+// if(WholeNumber % 2 == 0)
+// {
+//     Console.WriteLine("WholeNumber is Even");
+// }
+// else
+// {
+//     Console.WriteLine("WholeNumber is odd");
+// }
+
+
+
+
+// 4, Writh a program that takes two number and operator  as input and performs the corresponding operation.
+// for(int input = )
+
+
+
+
+// Write a program that takes three integers as input and finds the largest among them.
